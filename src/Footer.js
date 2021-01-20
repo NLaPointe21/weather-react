3b0963f363bda20d6a414f90d4129788da8b7d1b
NLaPointe21 <72793964+NLaPointe21@users.noreply.github.com>
2021-01-20 12:46:36 +0100

    Updated footer link format

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,11 @@ export default function Footer() {
   return (
     <div className="footer">
       <p className="source-code">
-        <a href="https://github.com/NLaPointe21/weather-react" target="_blank">
+        <a
+          href="https://github.com/NLaPointe21/weather-react"
+          rel="noreferrer"
+          target="_blank"
+        >
           Open-source code
         </a>{" "}
         by Nhakira LaPointe
